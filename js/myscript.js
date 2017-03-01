954353ae9d932cd608691b07f0a483cb346d3ff0
Natalia Rokicka <natalia9rokicka@gmail.com>
2017-03-01 20:39:50 +0000

    add file

---
+++ js/myscript.js
@@ -0,0 +1,15 @@
+function mySons(myNoun, myAdjective, myVerb, myAdverb) {
+    var result = "";
+   
+    result+= "My "+myAdjective+" "+myNoun+" "+myVerb+" very "+myAdverb+".";
+
+  
+  return result;
+}
+
+
+mySons("son", "older", "ran", "quickly");
+
+
+var mySons = [["Dima", 16], ["Les", 13]];
+mySons.push(["Andrij", 2]);
